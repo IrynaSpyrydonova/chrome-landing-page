95943ad72f32b74dbbd989179d7cfc117ad00dde
Hazem Bittar <mr.hazem@hotmail.fr>
2020-07-23 19:29:32 +0200

    Update Name function.js

--- scripts/user-actions/Name function.js
+++ scripts/user-actions/Name function.js
@@ -1,1 +1,22 @@
 
+// Get Name
+function getName() {
+  if (localStorage.getItem('name') === null) {
+    name.textContent = '[Enter Name]';
+  } else {
+    name.textContent = localStorage.getItem('name');
+  }
+}
+
+// Set Name
+function setName(e) {
+  if (e.type === 'keypress') {
+    // Make sure enter is pressed
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem('name', e.target.innerText);
+      name.blur();
+    }
+  } else {
+    localStorage.setItem('name', e.target.innerText);
+  }
+}
