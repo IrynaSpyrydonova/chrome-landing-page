e0ac8307cd4abe2be29debf813414db49c388c29
fatih <fatihkarakus@hotmail.com>
2020-07-24 11:18:26 +0200

    Fixed minor bugs

--- scripts/user-actions/Name function.js
+++ scripts/user-actions/Name function.js
@@ -1,25 +1,27 @@
-
 // Get Name
+const name = document.getElementById("name");
+
 function getName() {
-  if (localStorage.getItem('name') === null) {
-    name.textContent = '[Enter Name]';
+  if (localStorage.getItem("name") === null) {
+    name.textContent = "[Enter Name]";
   } else {
-    name.textContent = localStorage.getItem('name');
+    name.textContent = localStorage.getItem("name");
   }
 }
 
 // Set Name
 function setName(e) {
-  if (e.type === 'keypress') {
+  if (e.type === "keypress") {
     // Make sure enter is pressed
     if (e.which == 13 || e.keyCode == 13) {
-      localStorage.setItem('name', e.target.innerText);
+      localStorage.setItem("name", e.target.innerText);
       name.blur();
     }
   } else {
-    localStorage.setItem('name', e.target.innerText);
+    localStorage.setItem("name", e.target.innerText);
   }
 }
 
-name.addEventListener('keypress', setName);
-name.addEventListener('blur', setName);
+name.addEventListener("keypress", setName);
+name.addEventListener("blur", setName);
+getName();
